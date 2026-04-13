3643abfc4a2f3a0afc5af5b1ff99989566f26884
Reddy <kanchireddy.narayanareddy1@gmail.com>
2026-04-13 16:42:31 +0530

    hello world

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -3,3 +3,5 @@
 echo "Hello World"
 echo "chinna:: hii kanna, what are you doing"
 echo "Kanna:: im fine ,I hope you doing fine"
+echo "chinna:: kanna do you know devops?"
+echo "kanna:: yes chinna, i gone through online veidos"
