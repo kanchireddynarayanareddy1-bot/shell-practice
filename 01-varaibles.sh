277662123446e19f67d9fa2e917d9904a747bbc9
Reddy <kanchireddy.narayanareddy1@gmail.com>
2026-04-13 17:05:39 +0530

    hello world

---
+++ 01-varaibles.sh
@@ -0,0 +1,12 @@
+#!/bit/bash
+
+person1="ramesh"
+person2="suresh"
+
+echo "Hello World"
+echo "$person1:: hii $person2, what are you doing"
+echo "$person2:: im fine ,I hope you doing fine"
+echo "$person1:: $person2 do you know devops?"
+echo "$person2:: yes $person1, i gone through online videos"
+echo "$person1:: could you videos link which you will follow"
+echo "$person2:: yes i do "
