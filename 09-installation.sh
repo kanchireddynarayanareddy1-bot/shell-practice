9eb895b4faff197079514a73daa6bf16d349e247
Reddy <kanchireddy.narayanareddy1@gmail.com>
2026-04-15 11:41:15 +0530

    hello

--- 09-installation.sh
+++ 09-installation.sh
@@ -3,7 +3,7 @@
 UserId=$(id -u)
 
 if [ $UserId -ne 0 ]; then
-    echo "you can give sudo access to this script to run as root user""
+    echo "you can give sudo access to this script to run as root user"
 fi
 
 dnf install mysql -y
