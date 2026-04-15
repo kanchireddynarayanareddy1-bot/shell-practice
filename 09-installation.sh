418d2ebeadfed43f6da6358a60001bb1723f4c0d
Reddy <kanchireddy.narayanareddy1@gmail.com>
2026-04-15 11:25:08 +0530

    hello

--- 09-installation.sh
+++ 09-installation.sh
@@ -5,3 +5,12 @@ Number=$(id -u)
 if [ $Number -ne 0 ]; then
     echo "you can give sudo access to this script to run as root user""
 fi
+
+ dnf install mysql -y
+
+ if [ $? -ne 0 ]; then
+    echo "Installation failed, please check the error message"
+ else
+    echo "Installation successful"
+ fi 
+
