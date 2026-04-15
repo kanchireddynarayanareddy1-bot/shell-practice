7d24dfee7d7cedab708a7f04df4dbcf670c467f5
Reddy <kanchireddy.narayanareddy1@gmail.com>
2026-04-15 10:56:08 +0530

    hello

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -3,9 +3,9 @@
 Number=$1
 
 if [ $Number -gt 100 ]; then
-    echo "Number is $Number greater than 100"
+    echo "$Number is  greater than 100"
 elif [ $Number -eq 100 ]; then
-    echo "Number is $Number equal to 100"
+    echo "$Number is  equal to 100"
 else
-    echo "Number is $Number less than 100"
+    echo "$Number is  less than 100"
 fi
