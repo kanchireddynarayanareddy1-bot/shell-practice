904cf8b45f94de78c78b6a7138d842f458014a1c
Reddy <kanchireddy.narayanareddy1@gmail.com>
2026-04-17 12:58:49 +0530

    hello

---
+++ 13-loop.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+UserId=$(id -u) 
+R="\e[031m"
+G="\e[032m"
+Y="\e[033m"
+N="\e[0m"
+
+Folder="/var/log/shell-script"
+ScriptName=$(echo $0 | cut -d'.' -f1)
+LogFile="$Folder/$ScriptName.log"   
+mkdir -p $Folder
+echo "Script execution started at: $(date)" | tee -a $LogFile   
+
+if [ $UserId -ne 0 ]; then
+    echo "you can give sudo access to this script to run as root user"
+    exit 1
+fi
+function validate (){
+   if [ $1 -ne 0 ]; then
+    echo -e "Installation $2 $R failed $N" | tee -a $LogFile
+else
+    echo -e "Installation $2 $G successful $N" | tee -a $LogFile
+fi 
+}
+for package in $@ do
+    dnf list installed $package 
+    if [ $? -ne 0 ]; then
+        {
+            dnf install $package -y &>>$LogFile
+            validate $? "$package"
+        }
+    else
+        echo -e "$package already exist ... $Y SKIPPING $N" | tee -a $LogFile
+    fi
+done
