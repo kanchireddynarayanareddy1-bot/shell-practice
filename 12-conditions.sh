084edfa55fe4e4bff6617079e6b5974d1bb8e6ca
Reddy <kanchireddy.narayanareddy1@gmail.com>
2026-04-17 10:45:59 +0530

    hello

--- 12-conditions.sh
+++ 12-conditions.sh
@@ -2,13 +2,18 @@
 R="\e[31m"
 G="\e[32m"
 userId=$(id -u)
+Folder="/var/log/shell-script"
+ScriptName=$(echo $0 | cut -d'.' -f1)
+LogFile="$Folder/$ScriptName.log"
 if [ $userId -ne 0 ]; then
     echo "you can give sudo access to this script to run as root user"
     exit 1 
 fi
-dnf install mysql -y
+mkdir -p $Folder
+echo "Script execution started at: $(date)" | tee -a $LogFile
+dnf install mysql -y &>>$LogFile
  if [ $? -ne 0 ]; then
-    echo "Mysql Installation..."$R failed $N "
+    echo "Mysql Installation..."$R failed $N" 
 
 else
     echo "Mysql Installation..."$G successful $N "
