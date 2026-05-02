a86ec13346e58990f87ef80d18935310fbbaff86
Reddy <kanchireddy.narayanareddy1@gmail.com>
2026-05-02 11:23:59 +0530

    hello

---
+++ conditions.sh
@@ -0,0 +1,10 @@
+!#/bin/bash
+
+Number=$@
+if [ $Number -gt 100 ]; then
+    echo "$Number is  greater than 100"
+elif [ $Number -eq 100 ]; then
+    echo "$Number is  equal to 100"
+else
+    echo "$Number is  less than 100"
+fi
