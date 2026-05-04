6d71cdb1ca311901bdc701eb32e065b8ea0136f6
Reddy <kanchireddy.narayanareddy1@gmail.com>
2026-05-04 22:45:05 +0530

    main

---
+++ 17-auto-instances.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+AMI_ID="ami-0220d79f3f480ecf5"
+SG_ID="sg-031674407f289d035"
+INSTANCE_TYPE="t2.micro"
+
+
+for instance in $@
+do
+ INSTANCE_ID=$(aws ec2 run-instances  --image-id $AMI_ID  --instance-type $INSTANCE_TYPE--security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value="'$instance'"}]' --query 'Instances[0].InstanceId' --output text')
+ if [ $instance != "frontend" ]; then
+    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[9].Instances[0].PrivateIpAddress" 
+  --output text)
+ else
+    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" 
+  --output text)
+ fi 
+
+done
+
+echo "$instance: $IP"
