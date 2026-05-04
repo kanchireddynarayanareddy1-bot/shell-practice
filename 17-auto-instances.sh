adbf74eb1672175915aa24b4de390208a1d57409
Reddy <kanchireddy.narayanareddy1@gmail.com>
2026-05-04 23:00:39 +0530

    main

--- 17-auto-instances.sh
+++ 17-auto-instances.sh
@@ -7,11 +7,11 @@ INSTANCE_TYPE="t2.micro"
 
 for instance in $@
 do
- INSTANCE_ID=$(aws ec2 run-instances  --image-id $AMI_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value="'$instance'"}]' --query 'Instances[0].InstanceId' --output text')
-
+ INSTANCE_ID=$(aws ec2 run-instances  --image-id $AMI_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text')
 
  if [ $instance != "frontend" ]; then
     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+
  else
     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
  fi 
